896e335c3ea1f4c2721bf52effe6270accbd4349
dheeraj1341999 <dheerajreddi13@gmail.com>
2021-03-31 09:56:56 +0530

    first commit

---
+++ project/src/test/java/com/cg/es/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
